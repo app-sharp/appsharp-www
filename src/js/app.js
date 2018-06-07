7ba45de9850917316f717a2feca2370d615cf151
eliwilliamson <eliwilliamson@gmail.com>
2018-06-07 16:25:28 -0400

    fix mobile products page link

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,15 @@
 var winWidth = $(window).width();
 
-if ((winWidth <= 767) && ($('.landing').length)) {
-  $("nav .dropdown").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#products").offset().top
-    }, 1000);
-  });
+if (winWidth <= 767) {
+  if ($('.landing').length) {
+    $("nav .dropdown").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#products").offset().top
+      }, 1000);
+    });
+  } else {
+    $("nav .dropdown").click(function() {
+      window.location.href = "/products/";
+    });
+  }
 }
